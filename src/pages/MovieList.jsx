7e9fd7b794cadfe82fa6f30190ab7e19cbad4bf2
Bashar Umar <basharu83@gmail.com>
2025-03-16 15:18:56 +0100

    Initial commit

---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MovieList() {
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">
+          <h2 className="text-3xl font-bold tracking-tight">Popular Movies</h2>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default MovieList;
---
+++ src/routes/AllRoutes.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+
+function AllRoutes() {
+  return (
+    <div className="dark:bg-darkbg">
+      <Routes>
+        {/* <Route path="/" element={<App />} /> */}
+        {/* <Route
+          path=""
+          element={<MovieList apiPath="movie/now_playing" title="Home" />}
+        />
+        <Route path="movie/:id" element={<MovieDetail />} />
+        <Route
+          path="movies/popular"
+          element={<MovieList apiPath="movie/popular" title="Popular" />}
+        />
+        <Route
+          path="movies/top"
+          element={<MovieList apiPath="movie/top_rated" title="Top Rated" />}
+        />
+        <Route
+          path="movies/upcoming"
+          element={<MovieList apiPath="movie/upcoming" title="Upcoming" />}
+        />
+        <Route path="search" element={<Search apiPath="search/movie" />} />
+        <Route path="*" element={<PageNotFound />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default AllRoutes;
