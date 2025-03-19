ac866fb6b864195b91761e7041d2cada88654052
Bashar Umar <basharu83@gmail.com>
2025-03-19 04:45:10 +0100

    added poster path

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Card from "../components/Card";
 
 function MovieList() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_MOVIE_API_KEY;
+    const fetchMovies = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    };
+    fetchMovies();
+  }, []);
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap other:justify-evenly">
-          <Card />
+          {movies.map((movie) => {
+            console.log(movie);
+            return <Card key={movie.id} movie={movie} />;
+          })}
         </div>
       </section>
     </main>
